4c1390fbb9cef76c37d2696e9b0ae50b15eb870b
Dan Leonard <daniel@danleonard.us>
2018-05-20 03:32:17 -0500

    IT FUCKING WORKS HOLY SHIT

--- search-keybindings.js
+++ search-keybindings.js
@@ -1,5 +1,5 @@
-var search_keybindings_resultLinks;
-var search_keybindings_currentIndex;
+var search_keybindings_resultLinks = [];
+var search_keybindings_currentIndex = 0;
 var search_keybindings_searchBar;
 var search_keybindings_keyUp = 75;
 var search_keybindings_keyDown = 74;
@@ -15,92 +15,129 @@ function selectResult(resultNum) {
 	search_keybindings_resultLinks[resultNum].focus();
 }
 
-function indexResults() {
-	var resultListPane = document.getElementById("rso");
-	var resultList = resultListPane.getElementsByClassName("h3");
-
-	search_keybindings_resultLinks = resultList.getElementByTagName("a");
-	console.log(search_keybindings_resultLinks);
-	search_keybindings_searchBar = document.getElementById("lst-ib");
-}
-
 function moveUp() {
-	if (search_keybindings_currentIndex < 0) {
+	if (search_keybindings_currentIndex <= 0) {
+		console.log("└ EOL");
 		return;
 	}
-
+	console.log("│ current index is " + search_keybindings_currentIndex);
 	search_keybindings_currentIndex--;
+	console.log("└ targeting " + search_keybindings_currentIndex);
 	selectResult(search_keybindings_currentIndex);
 }
 
 function moveDown() {
-	if (search_keybindings_currentIndex >= search_keybindings_resultLinks.length) {
+	if (search_keybindings_currentIndex >= search_keybindings_resultLinks.length - 1) {
+		console.log("└ EOL");
 		return;
 	}
 
+	console.log("│ current index is " + search_keybindings_currentIndex);
 	search_keybindings_currentIndex++;
+	console.log("└ targeting " + search_keybindings_currentIndex);
 	selectResult(search_keybindings_currentIndex);
 }
 
 function moveHigh() {
+	console.log("│ current index is " + search_keybindings_currentIndex);
 	search_keybindings_currentIndex = 0;
+	console.log("└ targeting " + search_keybindings_currentIndex);
 	selectResult(search_keybindings_currentIndex);
 }
 
 function moveMiddle() {
+	console.log("│ current index is " + search_keybindings_currentIndex);
 	var mid = Math.floor(search_keybindings_resultLinks.length / 2)
 
 	search_keybindings_currentIndex = mid;
+	console.log("└ targeting " + search_keybindings_currentIndex);
 	selectResult(search_keybindings_currentIndex);
 }
 
-function moveLoLow() {
+function moveLow() {
+	console.log("│ current index is " + search_keybindings_currentIndex);
 	search_keybindings_currentIndex = search_keybindings_resultLinks.length - 1;
+	console.log("└ targeting " + search_keybindings_currentIndex);
 	selectResult(search_keybindings_currentIndex);
 }
 
 function selectSearchBar() {
 	search_keybindings_searchBar.focus();
+	console.log("└ targeting search bar");
 }
 
+/*
 function escapeSearchBar() {
 	selectResult(search_keybindings_currentIndex);
+	console.log("└ exiting search bar");
 }
+*/
 
 window.addEventListener("keydown", checkKeyPressed, false);
  
 function checkKeyPressed(e) {
 	console.log("Key " + e.keyCode + " pressed.");
 
+	let ae = document.activeElement;
+	console.log("│ Current active element is:");
+	console.log(ae);
+	let loc = search_keybindings_resultLinks.indexOf(ae);
+	if(loc >= 0) {
+		search_keybindings_currentIndex = loc;
+	} 
+	else if (ae.isSameNode(search_keybindings_searchBar)) {
+		console.log("└ Search bar active, exiting.");
+		return;
+	}
+
 	switch (e.keyCode) {
 		case search_keybindings_keyDown:
-			console.log("└ Key matches down key.");
+			console.log("│ Key matches down key.");
 			moveDown();
 			break;
 		case search_keybindings_keyUp:
-			console.log("└ Key matches up key.");
+			console.log("│ Key matches up key.");
 			moveUp();
 			break;
 		case search_keybindings_keySearch:
-			console.log("└ Key matches search key.");
+			console.log("│ Key matches search key.");
 			selectSearchBar();
 			break;
+		/*
 		case search_keybindings_keyEsc:
-			console.log("└ Key matches escape key.");
+			console.log("│ Key matches escape key.");
 			escapeSearchBar();
 			break;
+		*/
 		case search_keybindings_keyHigh:
-			console.log("└ Key matches high key.");
+			console.log("│ Key matches high key.");
 			moveHigh();
 			break;
 		case search_keybindings_keyMiddle:
-			console.log("└ Key matches middle key.");
+			console.log("│ Key matches middle key.");
 			moveMiddle();
 			break;
 		case search_keybindings_keyLow:
-			console.log("└ Key matches low key.");
+			console.log("│ Key matches low key.");
 			moveLow();
 			break;
 	}
+}
+
+/*
+document.addEventListener("DOMContentLoaded", indexResults()) {
+    console.log("DOM fully loaded and parsed");
+});
+*/
+{
+let resultListPane = document.getElementById("rso");
+let resultList = resultListPane.getElementsByTagName("h3");
+
+for (var i = 0, len = resultList.length; i < len; i++) {
+	search_keybindings_resultLinks.push(resultList[i].getElementsByTagName("a")[0]);
+}
+
+console.log(search_keybindings_resultLinks);
+search_keybindings_searchBar = document.getElementById("lst-ib");
 }
 
